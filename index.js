3f357f2802cb39e73ca982373aae77a7bc602b30
miro <fumiya116059@gmail.com>
2026-05-25 14:29:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import Dashboard from '../components/Dashboard'
+
+export default function Home(){
+  return (
+    <main className="container">
+      <Hero />
+      <Features />
+      <Dashboard />
+    </main>
+  )
+}
